212544c8aa6877fd5d884538706d1f5cb89974b5
Soulergonote <110846563+Soulergonote@users.noreply.github.com>
2024-04-20 14:06:01 +0200

    first bot

--- app.py
+++ app.py
@@ -3,29 +3,29 @@
 # This program is dedicated to the public domain under the CC0 license.
 
 """
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
+First, a few callback functions are defined. Then, those functions are passed to
 the Application and registered at their respective places.
 Then, the bot is started and runs until we press Ctrl-C on the command line.
 
-
 Usage:
-
-```python
-python echobot.py
-```
-
-Press Ctrl-C on the command line to stop the bot.
-
+Example of a bot-user conversation using ConversationHandler.
+Send /start to initiate the conversation.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
 """
 
 import os
 import logging
 
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
-
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -43,39 +43,74 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
+COUNTRY, QUESTION = range(2)
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Starts the conversation and asks the user about the desired country."""
+    reply_keyboard = [["Switzerland 🇨🇭", "France 🇫🇷", "Germany 🇩🇪", "Italy 🇮🇹"]]
+
+    await update.message.reply_text(
+        "Hello, I'm WelcomeBot, nice to meet you. I'll help you get settled in a new country!\n\n"
+        "Select the country you're interested in.",
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Which country?"
+        ),
     )
 
-
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
+    return COUNTRY
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
+async def country(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the selected country and asks for questions."""
+    user = update.message.from_user
+    logger.info(f"Country of {user} : {update.message.text}")
+    await update.message.reply_text(
+        f"{update.message.text},  awesome!\n"
+        "Tell me, what do you want to know about your installation there?",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+    
+    ### INSTANCIER LE MODELE ICI ###
+
+    return QUESTION
+
+
+async def question(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the country and asks for a question."""
+    user = update.message.from_user
     await update.message.reply_text(update.message.text)
+
+    return QUESTION
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text(
+        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
 
 
 def main() -> None:
-    """Start the bot."""
+    """Run the bot."""
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(TELEGRAM_KEY).build()
+    
+    # Add conversation handler
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            COUNTRY: [MessageHandler(filters.Regex("^(Switzerland 🇨🇭|France 🇫🇷|Germany 🇩🇪|Italy 🇮🇹)$"), country)],
+            QUESTION: [MessageHandler(filters.TEXT & ~filters.COMMAND, question)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
 
-    # on different commands - answer in Telegram
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
-
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(conv_handler)
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
